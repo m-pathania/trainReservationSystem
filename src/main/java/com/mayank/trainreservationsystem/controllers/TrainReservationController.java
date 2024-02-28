ef2c46b3b80c7bd01799e91a4b38c4863e50709d
Mayank Pathania <mayank.pathania@clear.in>
2024-02-29 04:16:35 +0530

    added-controller

--- src/main/java/com/mayank/trainreservationsystem/controllers/TrainReservationController.java
+++ src/main/java/com/mayank/trainreservationsystem/controllers/TrainReservationController.java
@@ -1,14 +1,33 @@
 package com.mayank.trainreservationsystem.controllers;
 
+import com.mayank.trainreservationsystem.requests.TicketBookingRequest;
+import com.mayank.trainreservationsystem.responses.TicketBookingResponse;
+import com.mayank.trainreservationsystem.services.TrainReservationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Validated
 @RestController
 @RequestMapping(path = "/v1")
+@RequiredArgsConstructor
 public class TrainReservationController {
-    @GetMapping("/ping")
-    public String ping() {
-        return "pong";
+    private final TrainReservationService trainReservationService;
+
+    @PostMapping("/book_ticket")
+    public TicketBookingResponse bookTicket(@RequestBody @Valid TicketBookingRequest request) {
+        if (Objects.isNull(request.getBookingDate())) {
+            request.setBookingDate(LocalDate.now());
+        }
+
+        return trainReservationService.bookTicket(request);
     }
 }
