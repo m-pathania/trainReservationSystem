8412a26b61a53a89f762c7dbbe28f2f21de4ffdc
Mayank Pathania <mayank.pathania@clear.in>
2024-02-28 20:09:02 +0530

    using-constants

---
+++ src/main/java/com/mayank/trainreservationsystem/constants/Entities.java
@@ -0,0 +1,14 @@
+package com.mayank.trainreservationsystem.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Entities {
+    public static final String TRAIN = "train";
+    public static final String TRAIN_ROUTE = "train_route";
+    public static final String TRAIN_STATION = "train_station";
+    public static final String TRAIN_SEAT = "train_seat";
+    public static final String BOOKING_INFO = "booking_info";
+    public static final String USER_INFO = "user_info";
+}
---
+++ src/main/java/com/mayank/trainreservationsystem/constants/Fields.java
@@ -0,0 +1,24 @@
+package com.mayank.trainreservationsystem.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Fields {
+    public static final String NAME = "name";
+    public static final String TRAIN_ID = "train_id";
+    public static final String STATION_ID = "station_id";
+    public static final String ORDER = "order";
+    public static final String ROUTE_ID = "route_id";
+    public static final String SECTION_NAME = "section";
+    public static final String DATE = "date";
+    public static final String IS_BOOKED = "is_booked";
+    public static final String SEAT_ID = "seat_id";
+    public static final String AMOUNT = "amount";
+    public static final String USER_ID = "user_id";
+    public static final String BOOKED_AT = "booked_at";
+    public static final String STATUS = "status";
+    public static final String EMAIL_ID = "email_id";
+    public static final String FIRSTNAME = "firstname";
+    public static final String LASTNAME = "lastname";
+}
