a79a2d633426d2a4ef90310023970daa30eeb97e
Mayank Pathania <mayank.pathania@clear.in>
2024-02-28 17:39:39 +0530

    dummy-comment

--- src/main/java/com/mayank/trainreservationsystem/TrainReservationSystemApplication.java
+++ src/main/java/com/mayank/trainreservationsystem/TrainReservationSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrainReservationSystemApplication {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         SpringApplication.run(TrainReservationSystemApplication.class, args);
     }
 
